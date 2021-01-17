b1994d7e293ccf8aba87d11b029d09a5a40cbd8f
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2021-01-17 12:40:07 +0100

    first changes at set_timer_page
    added some buttons

--- lib/set_timer_page.dart
+++ lib/set_timer_page.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 
-class SetTimerPage extends StatelessWidget {
+class SetTimerPage extends StatefulWidget {
+  @override
+  _SetTimerState createState() => _SetTimerState();
+}
+
+class _SetTimerState extends State<SetTimerPage> {
+  int _setCountdownMin = 1;
+  int _setCountdownSec = 0;
+
+  int _timeSetter() {
+    setState(() {
+      return 1;
+    });
+    return 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('RoundONE'),
-        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          OutlinedButton(
+              child: Text('$_setCountdownMin:$_setCountdownSec'),
+              onPressed: () {}),
+          OutlinedButton(child: Text('Round Time'), onPressed: () {}),
+          OutlinedButton(child: Text('Outlinebutton'), onPressed: () {}),
+          OutlinedButton(child: Text('Outlinebutton'), onPressed: () {}),
+        ],
       ),
     );
   }
