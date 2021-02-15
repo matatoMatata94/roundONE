d01b62df4dc8f04768c932bb29b98281d1930307
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2021-02-15 16:29:30 +0100

    Update timer_page

--- lib/pages/timer_page.dart
+++ lib/pages/timer_page.dart
@@ -26,31 +26,47 @@ class _TimerPageState extends State<TimerPage> {
         title: Text('TIMER'),
       ),
       body: Center(
-        child: CircularCountDownTimer(
-          duration: widget.countdownBeforeRound.inSeconds,
-          controller: CountDownController(),
-          width: MediaQuery.of(context).size.width / 1.25,
-          height: MediaQuery.of(context).size.height / 2,
-          color: Colors.grey[300],
-          fillColor: Colors.redAccent[200],
-          backgroundColor: Colors.red[600],
-          strokeWidth: 50.0,
-          strokeCap: StrokeCap.round,
-          textStyle: TextStyle(
-              fontSize: 80.0, color: Colors.white, fontWeight: FontWeight.bold),
-          textFormat: CountdownTextFormat.MM_SS,
-          isReverse: true,
-          isReverseAnimation: true,
-          isTimerTextShown: true,
-          autoStart: true,
-          onStart: () {
-            print('Countdown Started');
-          },
-          onComplete: () {
-            print('Countdown Ended');
-          },
+        child: circularCountDownTimer(
+          context,
+          widget.countdownBeforeRound,
+          widget.durationOfaRound,
+          widget.numberOfRounds,
+          widget.durationOfaPause,
         ),
       ),
+    );
+  }
+
+  CircularCountDownTimer circularCountDownTimer(
+    BuildContext context,
+    Duration countdownBeforeRound,
+    Duration durationOfaRound,
+    int numberOfRounds,
+    Duration durationOfaPause,
+  ) {
+    return CircularCountDownTimer(
+      duration: widget.countdownBeforeRound.inSeconds,
+      controller: CountDownController(),
+      width: MediaQuery.of(context).size.width / 1.25,
+      height: MediaQuery.of(context).size.height / 2,
+      color: Colors.grey[300],
+      fillColor: Colors.redAccent[200],
+      backgroundColor: Colors.red[600],
+      strokeWidth: 50.0,
+      strokeCap: StrokeCap.round,
+      textStyle: TextStyle(
+          fontSize: 80.0, color: Colors.white, fontWeight: FontWeight.bold),
+      textFormat: CountdownTextFormat.MM_SS,
+      isReverse: true,
+      isReverseAnimation: true,
+      isTimerTextShown: true,
+      autoStart: true,
+      onStart: () {
+        print('Countdown Started');
+      },
+      onComplete: () {
+        print('Countdown Ended');
+      },
     );
   }
 
