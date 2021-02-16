f7a72d6f75bd612a7d2204d2537d0721878a80e0
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2021-02-16 14:56:51 +0100

    Update timer_page

--- lib/pages/timer_page.dart
+++ lib/pages/timer_page.dart
@@ -5,7 +5,7 @@ class TimerPage extends StatefulWidget {
   TimerPage({
     this.countdownBeforeRound,
     this.durationOfaRound,
-    this.numberOfRounds,
+    this.numberOfRounds = 1,
     this.durationOfaPause,
   });
 
@@ -66,20 +66,60 @@ class _TimerPageState extends State<TimerPage> {
       },
       onComplete: () {
         print('Countdown Ended');
+        for (int i = numberOfRounds; i > 0; i--)
+          CircularCountDownTimer(
+            duration: widget.durationOfaRound.inSeconds,
+            controller: CountDownController(),
+            width: MediaQuery.of(context).size.width / 1.25,
+            height: MediaQuery.of(context).size.height / 2,
+            color: Colors.grey[300],
+            fillColor: Colors.redAccent[200],
+            backgroundColor: Colors.red[600],
+            strokeWidth: 50.0,
+            strokeCap: StrokeCap.round,
+            textStyle: TextStyle(
+                fontSize: 80.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold),
+            textFormat: CountdownTextFormat.MM_SS,
+            isReverse: true,
+            isReverseAnimation: true,
+            isTimerTextShown: true,
+            autoStart: true,
+            onStart: () {
+              print('Round Started');
+            },
+            onComplete: () {
+              print('Round Ended');
+              CircularCountDownTimer(
+                duration: widget.durationOfaRound.inSeconds,
+                controller: CountDownController(),
+                width: MediaQuery.of(context).size.width / 1.25,
+                height: MediaQuery.of(context).size.height / 2,
+                color: Colors.grey[300],
+                fillColor: Colors.redAccent[200],
+                backgroundColor: Colors.red[600],
+                strokeWidth: 50.0,
+                strokeCap: StrokeCap.round,
+                textStyle: TextStyle(
+                    fontSize: 80.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+                textFormat: CountdownTextFormat.MM_SS,
+                isReverse: true,
+                isReverseAnimation: true,
+                isTimerTextShown: true,
+                autoStart: true,
+                onStart: () {
+                  print('Pause Started');
+                },
+                onComplete: () {
+                  print('Pause Ended');
+                },
+              );
+            },
+          );
       },
     );
-  }
-
-  int chosenCountdown(
-    Duration countdownBeforeRound,
-    Duration durationOfaRound,
-    int numberOfRounds,
-    Duration durationOfaPause,
-  ) {
-    if (countdownBeforeRound.inSeconds != 0) {
-      return countdownBeforeRound.inSeconds;
-    } else {
-      return durationOfaRound.inSeconds;
-    }
   }
 }
